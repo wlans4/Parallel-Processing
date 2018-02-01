ec92ec62821ee6d9d632d1332bd627ece7866b28
Wyatt <wlans4@vt.edu>
2018-02-01 17:06:03 -0500

    Homework 1 being added, as well as the Homework directory

---
+++ Homework/HW01/gcd.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+
+int main(){
+
+
+int a;
+int b;
+int gcd; 
+
+printf("Enter the first number: ");
+scanf("%d", &a);
+printf("Enter the second number: ");
+scanf("%d", &b);
+
+    if (b == 0){
+    printf("The GCD of %d and %d is %d", a, b, a);
+        }
+
+    else {
+        for (int i = 1; i < a && i <= b; i++){
+            if (a%i == 0 && b%i == 0){
+                gcd = i;
+                     }//if mod   
+            }//for i
+        } //else 
+printf("The greatest common denominator of %d and %d is %d\n", a, b, gcd);
+}//main
