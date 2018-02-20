39a575da466878ae90f467354bc602345d6d221f
Wyatt <wlans4@vt.edu>
2018-02-20 16:49:21 -0500

    attempt at assignment

--- L11/spi.c
+++ L11/spi.c
@@ -4,15 +4,25 @@
 
 int main(int argc, char **argv) {
 
-  //need running tallies
-  long long int Ntotal;
-  long long int Ncircle;
 
-  //seed random number generator
-  double seed = 1.0;
-  srand48(seed);
+    MPI_Init(&argc, &argv);
+    int rank, size;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-  for (long long int n=0; n<1000000;n++) {
+    int sourceRank = rank -1;
+    if (rank == 0) sourceRank = size -1;
+    int N = 1;
+    
+    //need running tallies
+    long long int Ntotal;
+    long long int Ncircle;
+
+    //seed random number generator
+    double seed = 1.0;
+    srand48(rank);
+
+  for (long long int n=0; n<10000;n++) {
     //gererate two random numbers
     double rand1 = drand48(); //drand48 returns a number between 0 and 1
     double rand2 = drand48();
@@ -25,9 +35,12 @@ int main(int argc, char **argv) {
     Ntotal++;
   }
 
-  double pi = Ncircle/ (double) Ntotal;
+  double localpi = Ncircle/ (double) Ntotal;
+  float float worldpisum +=localpi;
+  MPI_Reduce(&localpi, &worldpisum, 1, MPI_FLOAT, 0, MPI_COMM_WORLD);
 
-  printf("Our estimate of pi is %f \n", pi);
 
+
+  MPI_Finalize();
   return 0;
 }
